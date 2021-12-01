4f27bd1faf8772c78d429e3d1faeeb97ccbdfbee
Bohdan Widla <bohdan@bohdanwidla.pl>
2021-12-01 09:12:18 +0100

    Update Day 1: fix typos

--- 01/d01.py
+++ 01/d01.py
@@ -24,10 +24,10 @@ test_measurements_adjusted = [sum(test_measurements[i:i+3]) for i in range(len(t
 
 print('Tests...')
 print("Checking number of depth increases in test data: ", count_depth_increases(test_measurements) == 7)
-print("Checking number of depth increases in test data, secondd method: ", count_depth_increases(test_measurements_adjusted) == 5)
-print('And now with functional approach...')
+print("Checking number of depth increases in test data, second method: ", count_depth_increases(test_measurements_adjusted) == 5)
+print('And now with a more functional approach...')
 print("Checking number of depth increases in test data: ", count_depth_increases_functional(test_measurements, 1) == 7)
-print("Checking number of depth increases in test data, secondd method: ", count_depth_increases_functional(test_measurements, 3) == 5)
+print("Checking number of depth increases in test data, second method: ", count_depth_increases_functional(test_measurements, 3) == 5)
 print('---------------------')
 
 print('Solution...')
@@ -36,7 +36,7 @@ with open('inp', mode='r') as inp:
     measurements_adjusted = [sum(measurements[i:i+3]) for i in range(len(measurements))]
     print("Checking number of depth increases in input data: ", count_depth_increases(measurements))
     print("Checking number of depth increases in input data, second method: ", count_depth_increases(measurements_adjusted))
-    print('And now with functional approach...')
+    print('And now with a more functional approach...')
     print("Checking number of depth increases in test data: ", count_depth_increases_functional(measurements, 1))
-    print("Checking number of depth increases in test data, secondd method: ", count_depth_increases_functional(measurements, 3))
+    print("Checking number of depth increases in test data, second method: ", count_depth_increases_functional(measurements, 3))
     
