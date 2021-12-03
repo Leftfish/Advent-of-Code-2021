962329cc77e1f5f0398f402119151f5bc9829822
Bohdan Widla <bohdan@bohdanwidla.pl>
2021-12-03 13:03:54 +0100

    Refactor day 3: remove redundant '0b' prefixes

--- 03/d03.py
+++ 03/d03.py
@@ -11,19 +11,22 @@ def count_ones(bits: list, bit_size: int) -> defaultdict:
                 positions[pos] += 1
     return positions
 
+
 def calculate_power(bits: list, bit_size: int) -> int:
-    gamma = '0b'
+    gamma = ''
     bit_frequency = count_ones(bits, bit_size)
+
     for i in range(bit_size):
         if bit_frequency[i] > len(bits)/2:
             gamma += '1'
         else:
             gamma += '0'
-    epsilon = '0b' + ''.join('1' if digit == '0' else '0' for digit in gamma[2:])
+    epsilon = ''.join('1' if digit == '0' else '0' for digit in gamma)
 
-    return int(gamma,2) * int(epsilon,2)
+    return int(gamma, 2) * int(epsilon, 2)
 
-def calculate_rating(candidates: set, bit_frequency: dict, pos: int, mode: int) -> set:
+
+def filter_life_support_candidates(candidates: set, bit_frequency: dict, pos: int, mode: int) -> set:
     filtered_candidates = set()
 
     if mode == 1:
@@ -37,8 +40,9 @@ def calculate_rating(candidates: set, bit_frequency: dict, pos: int, mode: int) 
 
     return filtered_candidates
 
+
 def calculate_life_support(bits: list, bit_size: int) -> int:
-    oxygen, co2 = 1,0
+    oxygen, co2 = 1, 0
 
     positions_oxy = count_ones(bits, bit_size)
     positions_co2 = count_ones(bits, bit_size)
@@ -47,17 +51,17 @@ def calculate_life_support(bits: list, bit_size: int) -> int:
 
     for bit_position in range(bit_size):
         if len(candidates_oxy) > 1:
-            candidates_oxy = calculate_rating(candidates_oxy, positions_oxy, bit_position, oxygen)
+            candidates_oxy = filter_life_support_candidates(candidates_oxy, positions_oxy, bit_position, oxygen)
             positions_oxy = count_ones(candidates_oxy, bit_size)
-        
-        if len(candidates_co2) > 1:
-            candidates_co2 = calculate_rating(candidates_co2, positions_co2, bit_position, co2)
-            positions_co2 = count_ones(candidates_co2, bit_size)
-        
-    oxy = '0b' + candidates_oxy.pop()
-    co2 = '0b' + candidates_co2.pop()
 
-    return int(oxy, 2) *  int(co2,2)
+        if len(candidates_co2) > 1:
+            candidates_co2 = filter_life_support_candidates(candidates_co2, positions_co2, bit_position, co2)
+            positions_co2 = count_ones(candidates_co2, bit_size)
+
+    oxy = candidates_oxy.pop()
+    co2 = candidates_co2.pop()
+
+    return int(oxy, 2) * int(co2, 2)
 
 
 test_data = '''00100
@@ -78,7 +82,6 @@ bits = test_data.splitlines()
 bit_size = len(bits[0])
 print('Power consumption:', calculate_power(bits, bit_size) == 198)
 print('Life support rating:', calculate_life_support(bits, bit_size) == 230)
-
 print('---------------------')
 
 print('Solution...')
