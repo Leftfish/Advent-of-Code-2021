4a4d54c6755e66c4a8fec54c23116077a85b087f
Bohdan Widla <bohdan@bohdanwidla.pl>
2021-12-03 14:11:57 +0100

    Refactor day 3: separate functions for part 2

--- 03/d03.py
+++ 03/d03.py
@@ -2,7 +2,7 @@ print('Day 3 of Advent of Code!')
 
 from collections import defaultdict
 from operator import ge, lt
-from typing import Callable, Dict, List, Set
+from typing import Callable, DefaultDict, List, Set
 
 
 def count_ones(bits: List[str], bit_size: int) -> defaultdict:
@@ -29,39 +29,28 @@ def calculate_power(bits: List[str], bit_size: int) -> int:
     return int(gamma, 2) * int(epsilon, 2)
 
 
-def filter_life_support_candidates(candidates: Set[str], bit_frequency: Dict[int, int], pos: int, comparator: Callable[[int, int], bool]) -> set:
-    filtered_candidates = set()
-    flag = '1' if comparator(bit_frequency[pos], len(candidates)/2) else '0'    
-
-    for number in candidates:
-        if number[pos] == flag:
-            filtered_candidates.add(number)
-
-    return filtered_candidates
+def filter_life_support_candidates(candidates: Set[str], bit_frequency: DefaultDict[int, int], pos: int, comparator: Callable[[int, int], bool]) -> set:
+    flag = '1' if comparator(bit_frequency[pos], len(candidates)/2) else '0'        
+    return set(number for number in candidates if number[pos] == flag)
+    #return set(filter(lambda number: number[pos] == flag, candidates))
 
 
-def calculate_life_support(bits: List[str], bit_size: int) -> int:
-    oxygen, co2 = ge, lt
-
-    positions_oxy = count_ones(bits, bit_size)
-    positions_co2 = count_ones(bits, bit_size)
-    candidates_oxy = set(bits)
-    candidates_co2 = set(bits)
+def get_life_support_rating(bits: List[str], bit_size: int, system: Callable[[int, int], bool]) -> int:
+    positions = count_ones(bits, bit_size)
+    candidates = set(bits)
 
     for bit_position in range(bit_size):
-        if len(candidates_oxy) > 1:
-            candidates_oxy = filter_life_support_candidates(candidates_oxy, positions_oxy, bit_position, oxygen)
-            positions_oxy = count_ones(candidates_oxy, bit_size)
+        if len(candidates) > 1:
+            candidates = filter_life_support_candidates(candidates, positions, bit_position, system)
+            positions = count_ones(candidates, bit_size)
+        else:
+            break
 
-        if len(candidates_co2) > 1:
-            candidates_co2 = filter_life_support_candidates(candidates_co2, positions_co2, bit_position, co2)
-            positions_co2 = count_ones(candidates_co2, bit_size)
+    return int(candidates.pop(), 2)
 
-    oxy = candidates_oxy.pop()
-    co2 = candidates_co2.pop()
 
-    return int(oxy, 2) * int(co2, 2)
-
+def calculate_life_support(bits: List[str], bit_size: int, oxygen: Callable[[int, int], bool], co2: Callable[[int, int], bool]):
+    return get_life_support_rating(bits, bit_size, oxygen) * get_life_support_rating(bits, bit_size, co2)
 
 test_data = '''00100
 11110
@@ -80,7 +69,7 @@ print('Tests...')
 bits = test_data.splitlines()
 bit_size = len(bits[0])
 print('Power consumption:', calculate_power(bits, bit_size) == 198)
-print('Life support rating:', calculate_life_support(bits, bit_size) == 230)
+print('Life support rating:', calculate_life_support(bits, bit_size, ge, lt) == 230)
 print('---------------------')
 
 print('Solution...')
@@ -88,4 +77,4 @@ with open('inp', mode='r') as inp:
     bits = inp.read().splitlines()
     bit_size = len(bits[0])
     print('Power consumption:', calculate_power(bits, bit_size))
-    print('Life support rating:', calculate_life_support(bits, bit_size))
+    print('Life support rating:', calculate_life_support(bits, bit_size, ge, lt))
