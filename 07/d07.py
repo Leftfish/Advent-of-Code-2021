4b41e8c6a87569cfbfc2d99fecf8a86bf45633af
Bohdan Widla <bohdan@bohdanwidla.pl>
2021-12-07 08:32:35 +0100

    Add typing hints to Day 7.

--- 07/d07.py
+++ 07/d07.py
@@ -1,37 +1,39 @@
+from typing import Callable, List
+
 print('Day 7 of Advent of Code!')
 
-def sum_range(a, b):
+def sum_range(a: int, b: int) -> int:
     n = abs(b - a)
-    return n * (n + 1) / 2
+    return int(n * (n + 1) / 2)
 
-def move_crabs(crabs, pos, cost_counter):
+def move_crabs(crabs: List[int], pos: int, cost_counter: Callable[[int, int], int]) -> int:
     return sum(cost_counter(crab, pos) for crab in crabs)
 
-def crabs_naive(crabs, max_position, cost_counter):
+def crabs_naive(crabs: List[int], cost_counter: Callable[[int, int], int]) -> int:
     minimum = None
-    for pos in range(max_position):
+    for pos in range(crabs[-1]):
         cost = move_crabs(crabs, pos, cost_counter)
         if not minimum or cost < minimum:
             minimum = cost
     return minimum
 
-def crabs_by_median(crabs):
-    median = sorted(crabs)[len(crabs)//2]
+def crabs_by_median(crabs: List[int]) -> int:
+    median = crabs[len(crabs)//2]
     return sum(abs(median - crab) for crab in crabs)
 
 raw_data = '''16,1,2,0,4,2,7,1,2,14'''
 
 print('Tests...')
-crabs = list(map(int, raw_data.split(',')))
-print('Cheap crabs:', int(crabs_naive(crabs, max(crabs), lambda a, b: abs(b - a))) == 37)
+crabs = sorted(list(map(int, raw_data.split(','))))
+print('Cheap crabs:', int(crabs_naive(crabs, lambda a, b: abs(b - a))) == 37)
 print('Cheap crabs (non naive):', int(crabs_by_median(crabs)) == 37)
-print('Expensive crabs:', (int(crabs_naive(crabs, max(crabs), sum_range)) == 168))
+print('Expensive crabs:', (int(crabs_naive(crabs, sum_range)) == 168))
 print('---------------------')
 
 print('Solution...')
 with open('inp', mode='r') as inp:
     raw_data = inp.read()
-    crabs = list(map(int, raw_data.split(',')))
-    print('Cheap crabs:', int(crabs_naive(crabs, max(crabs), lambda a, b: abs(b - a))))
+    crabs = sorted(list(map(int, raw_data.split(','))))
+    print('Cheap crabs:', int(crabs_naive(crabs, lambda a, b: abs(b - a))))
     print('Cheap crabs (non naive):', int(crabs_by_median(crabs)))
-    print('Expensive crabs:', (int(crabs_naive(crabs, max(crabs), sum_range))))
+    print('Expensive crabs:', (int(crabs_naive(crabs, sum_range))))
