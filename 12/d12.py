38930195a0562ecc644b5881a774e9405bf5ee4d
Bohdan Widla <bohdan@bohdanwidla.pl>
2021-12-12 13:23:44 +0100

    Refactor Day 12: dict uses sets now, paths to start filtered out

--- 12/d12.py
+++ 12/d12.py
@@ -2,12 +2,15 @@ from collections import defaultdict
 
 print('Day 12 of Advent of Code!')
 
+
 def make_graph(data):
-    graph = defaultdict(list)
+    graph = defaultdict(set)
     for line in data.splitlines():
-        parsed = line.split('-')
-        graph[parsed[0]].append(parsed[1])
-        graph[parsed[1]].append(parsed[0])
+        a, b = line.split('-')
+        if b != 'start':
+            graph[a].add(b)
+        if a != 'start':
+            graph[b].add(a)
     return graph
 
 def find_all_paths(graph, current, path_so_far, flag):
@@ -18,18 +21,16 @@ def find_all_paths(graph, current, path_so_far, flag):
     
     if current == 'end':
         yield path_so_far + [current]
-        
     else:
         for neigh in graph[current]:
-            if neigh == 'start':
-                pass
-            elif neigh.isupper() or neigh not in path_so_far:
+            if neigh.isupper() or neigh not in path_so_far:
                 yield from find_all_paths(graph, neigh, path_so_far + [neigh], flag)
             elif neigh.islower() and flag:
                 # if the function starts with flag set to True, it runs here to allow a single small cave
                 # to be visited twice. this line sets the flag back to False so no other small cave gets
                 # visited twice
                 yield from find_all_paths(graph, neigh, path_so_far + [neigh], False)
+
 
 raw_data = '''start-A
 start-b
