507e029c961cf14fcf09958fa59c2c4928539ecd
Bohdan Widla <bohdan@bohdanwidla.pl>
2021-12-18 17:51:54 +0100

    Day 18 add finding explosive pairs

--- 18/d18.py
+++ 18/d18.py
@@ -2,6 +2,7 @@ print('Day 18 of Advent of Code!')
 
 raw_data = ''''''
 
+EXPLOSIVE_DEPTH = 4
 
 class Node:
     def __init__(self, data, parent, depth=1):
@@ -21,14 +22,19 @@ class Node:
             self.val = data
             self.parent = parent
 
+    def is_explosive(self):
+        return self.depth > EXPLOSIVE_DEPTH
+
+    def has_only_leafs(self):
+        return (self.left and self.left.val != None) and (self.right and self.right.val != None)
+    
     def __repr__(self):
         return str(self.val) if self.val is not None else f'[{self.left},{self.right}]'
 
 
 
-def find_prev(node):    
+def find_prev(node: Node)  -> Node:    
     def find_rightmost_leaf(node):
-
         if node.val is not None:
             return node
         else:
@@ -36,29 +42,24 @@ def find_prev(node):
 
     current_parent = node.parent
 
-    print(f'I am {node}. My parent is {current_parent}')
-
+    # predecessor on the same level?
     if current_parent.left.val is not None and current_parent.left is not node:
-        print(f'My predecessor is on the same level')
         return current_parent.left.val
     
     else:
         prev_parent = current_parent.parent
-
+        # we tried going left but reached root - no predecessors!
         if current_parent.parent == None:
-            print(f'Reached root, no predecessors!')
             return
-        
+        # we went up to a 'right' node - looking in its 'left' neighbor
         elif current_parent == prev_parent.right:
-            print(f'Found proper node, looking in {prev_parent.left}')
             return find_rightmost_leaf(prev_parent.left)
-        
+        # we went up, not at root yet, let's go up until we're in a 'right' node
         else:
-            print(f'Recursing from {current_parent}')
             return find_prev(current_parent)
 
 
-def find_next(node):
+def find_next(node: Node) -> Node:
     
     def find_leftmost_leaf(node):
         if node.val is not None:
@@ -67,44 +68,48 @@ def find_next(node):
             return find_leftmost_leaf(node.left)
     
     current_parent = node.parent
-
-    print(f'I am {node}. My parent is {current_parent}')
    
     if current_parent.right.val is not None and current_parent.right is not node:
-        print(f'My successor is on the same level')
         return current_parent.right.val
     
     else:
         prev_parent = current_parent.parent
 
         if current_parent.parent == None:
-            print(f'Reached root, no successors!')
             return
         
         elif current_parent == prev_parent.left:
-            print(f'Found proper node, looking in {prev_parent.right}')
             return find_leftmost_leaf(prev_parent.right)
         
         else:
-            print(f'Recursing from {current_parent}')
             return find_next(current_parent)
 
-def printInorder(root):
-    if root:
-        printInorder(root.left)
-        # then print the data of node
-        print(root.val, end = ' ')
-        # now recur on right child
-        printInorder(root.right)
+def find_explosive_pair(node: Node) -> Node:
+    # in-order traversal until an explosive pair is found
+    current = node
+    stack = []
+         
+    while True:
+        if current is not None:
+            stack.append(current)
+            current = current.left
+ 
+        elif stack:
+            current = stack.pop()
+            if current.is_explosive() and current.has_only_leafs():
+                return current
+            current = current.right
+ 
+        else:
+            break
+      
+    return None
 
 
-
-l = [[[[1,1],[2,2]],[3,3]],[4,4]]
-l = [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+l = [[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]
 tree = Node(l, None)
 print(tree)
-
-printInorder(tree)
+print(find_explosive_pair(tree))
 
 #curr = tree.left.left.left
 #print(curr)
