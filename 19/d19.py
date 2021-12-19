8ecb1b695189315d6328783f706d42a00dbaa94f
Bohdan Widla <bohdan@bohdanwidla.pl>
2021-12-19 15:25:00 +0100

    Work in progress Day 19 (can find overlaps between scanners)

---
+++ 19/d19.py
@@ -0,0 +1,308 @@
+import re
+from collections import defaultdict
+from math import sqrt
+from itertools import product, combinations
+
+print('Day 19 of Advent of Code!')
+
+test_data = '''--- scanner 0 ---
+404,-588,-901
+528,-643,409
+-838,591,734
+390,-675,-793
+-537,-823,-458
+-485,-357,347
+-345,-311,381
+-661,-816,-575
+-876,649,763
+-618,-824,-621
+553,345,-567
+474,580,667
+-447,-329,318
+-584,868,-557
+544,-627,-890
+564,392,-477
+455,729,728
+-892,524,684
+-689,845,-530
+423,-701,434
+7,-33,-71
+630,319,-379
+443,580,662
+-789,900,-551
+459,-707,401
+
+--- scanner 1 ---
+686,422,578
+605,423,415
+515,917,-361
+-336,658,858
+95,138,22
+-476,619,847
+-340,-569,-846
+567,-361,727
+-460,603,-452
+669,-402,600
+729,430,532
+-500,-761,534
+-322,571,750
+-466,-666,-811
+-429,-592,574
+-355,545,-477
+703,-491,-529
+-328,-685,520
+413,935,-424
+-391,539,-444
+586,-435,557
+-364,-763,-893
+807,-499,-711
+755,-354,-619
+553,889,-390
+
+--- scanner 2 ---
+649,640,665
+682,-795,504
+-784,533,-524
+-644,584,-595
+-588,-843,648
+-30,6,44
+-674,560,763
+500,723,-460
+609,671,-379
+-555,-800,653
+-675,-892,-343
+697,-426,-610
+578,704,681
+493,664,-388
+-671,-858,530
+-667,343,800
+571,-461,-707
+-138,-166,112
+-889,563,-600
+646,-828,498
+640,759,510
+-630,509,768
+-681,-892,-333
+673,-379,-804
+-742,-814,-386
+577,-820,562
+
+--- scanner 3 ---
+-589,542,597
+605,-692,669
+-500,565,-823
+-660,373,557
+-458,-679,-417
+-488,449,543
+-626,468,-788
+338,-750,-386
+528,-832,-391
+562,-778,733
+-938,-730,414
+543,643,-506
+-524,371,-870
+407,773,750
+-104,29,83
+378,-903,-323
+-778,-728,485
+426,699,580
+-438,-605,-362
+-469,-447,-387
+509,732,623
+647,635,-688
+-868,-804,481
+614,-800,639
+595,780,-596
+
+--- scanner 4 ---
+727,592,562
+-293,-554,779
+441,611,-461
+-714,465,-776
+-743,427,-804
+-660,-479,-426
+832,-632,460
+927,-485,-438
+408,393,-506
+466,436,-512
+110,16,151
+-258,-428,682
+-393,719,612
+-211,-452,876
+808,-476,-593
+-575,615,604
+-485,667,467
+-680,325,-822
+-627,-443,-432
+872,-547,-609
+833,512,582
+807,604,487
+839,-516,451
+891,-625,532
+-652,-548,-490
+30,-46,-14'''
+
+def get_beacons(scanner: str):
+    beacons = []
+    for raw_beacon in scanner.splitlines():
+        beacon = tuple(map(int, raw_beacon.split(',')))
+        beacons.append(beacon)
+    return beacons
+
+def distance(this, other) -> float:
+    x1, y1, z1 = this
+    x2, y2, z2 = other
+    return sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+
+def get_distance_map(beacons_detected): # computes relative distances (3d) between pairs of beacons detected by a scanner
+    pairs = combinations(beacons_detected, 2)
+    distance_map = defaultdict(set)
+    for one, other in pairs:
+        dist = distance(one, other)
+        distance_map[one].add(dist)
+        distance_map[other].add(dist)
+    return distance_map
+
+class Scanner: # use class to calculate distance maps only once! then we'll figure out how to normalize this...
+    def __init__(self, id):
+        self.id = id
+        self.beacons = None
+        self.distance_map = None
+        self.x = None
+        self.y = None
+    
+    def __repr__(self):
+        return f'SCN {self.id}'
+
+    def get_beacons(self, scanner: str):
+        beacons = []
+        for raw_beacon in scanner.splitlines():
+            beacon = tuple(map(int, raw_beacon.split(',')))
+            beacons.append(beacon)
+        self.beacons = beacons
+
+    def distance(this, other) -> float:
+        x1, y1, z1 = this
+        x2, y2, z2 = other
+        return sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+
+    def get_distance_map(self): # computes relative distances (3d) between pairs of beacons detected by a scanner
+        pairs = combinations(self.beacons, 2)
+        distance_map = defaultdict(set)
+        for one, other in pairs:
+            dist = distance(one, other)
+            distance_map[one].add(dist)
+            distance_map[other].add(dist)
+        self.distance_map = distance_map
+
+def get_overlap_map(this_scanner, other_scanner):
+    beacon_pairs = product(this_scanner.beacons, other_scanner.beacons)
+    for this_beacon, other_beacon in beacon_pairs:
+        d_this = this_scanner.distance_map[this_beacon]
+        d_other = other_scanner.distance_map[other_beacon]
+        overl = len(d_this & d_other)
+        if overl >= 11:
+            print(f'{this_beacon} == {other_beacon}')
+
+def initiate_scanners(data):
+    regex = r'--- scanner (\d+) ---'
+    scanners = []
+    beacons = []
+    current = None
+    for line in data.splitlines():  
+        is_new_scanner = re.findall(regex, line)
+        
+        if is_new_scanner:
+            if current:
+                current.beacons = beacons
+                beacons = []
+            
+            new_scanner = Scanner(int(is_new_scanner[0]))
+            scanners.append(new_scanner) 
+            current = new_scanner
+            
+        elif line:
+            beacon = tuple(map(int, line.split(',')))
+            beacons.append(beacon)
+    
+    current.beacons = beacons
+
+    for scanner in scanners:
+        scanner.get_distance_map()
+    
+    return scanners
+
+scanners = initiate_scanners(test_data)
+
+s_pairs = combinations(scanners, 2)
+for one, other in s_pairs:
+    pass
+    print(one, other)
+    get_overlap_map(one, other)
+
+
+
+
+def rotate(x, y, z, i) -> tuple:
+    rotates = [(x, y, z),
+        (-y, x, z),
+        (-x, -y, z),
+        (y, -x, z),
+        (-z, y, x),
+        (-y, -z, x),
+        (z, -y, x),
+        (y, z, x),
+        (-x, y, -z),
+        (-y, -x, -z),
+        (x, -y, -z),
+        (y, x, -z),
+        (z, y, -x),
+        (-y, z, -x),
+        (-z, -y, -x),
+        (y, -z, -x),
+        (x, -z, y),
+        (z, x, y),
+        (-x, z, y),
+        (-z, -x, y),
+        (x, z, -y),
+        (-z, x, -y),
+        (-x, -z, -y),
+        (z, -x, -y)]
+    return rotates[i]
+
+def rotate_beacons(beacons):
+    rotated = defaultdict(list)
+    for beacon in beacons:
+
+        for i in range(24):
+            rotated[tuple(beacon)].append(rotate(*beacon, i))
+    return rotated
+            
+def lcs(this, other):
+    overlap = [[0 for _ in range(len(this) + 1)] for _ in range(len(other) + 1)]
+    for i in range(len(this)):
+        for j in range(len(other)):
+            if this[i] == other[j]:
+                overlap[i+1][j+1] = overlap[i][j] + 1
+            else:
+                overlap[i+1][j+1] = max(overlap[i][j+1], overlap[i+1][j])
+    res = []
+    i, j = len(this), len(other)
+    while i and j:
+        if overlap[i][j] == overlap[i - 1][j]:
+            i -= 1
+        elif overlap[i][j] == overlap[i][j - 1]:
+            j -= 1
+        else:
+            res.append(this[i - 1])
+            i -= 1
+            j -= 1
+    return res[::-1]
+
+print('Tests...')
+
+print('---------------------')
+
+print('Solution...')
+with open('inp', mode='r') as inp:
+    raw_data = inp.read()
